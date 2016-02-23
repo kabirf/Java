0deef070f6c4ea718d6b9fea19c31e8f01d1a962
Jahid Shohel <jahid.shohel@qlik.com>
2016-02-23 09:34:12 +0100

    added source folder

---
+++ src/com/company/User.java
@@ -0,0 +1,39 @@
+package com.company;
+
+/**
+ * Created by Administrator on 23/02/16.
+ */
+public class User {
+
+    private String firstName;
+    private String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
